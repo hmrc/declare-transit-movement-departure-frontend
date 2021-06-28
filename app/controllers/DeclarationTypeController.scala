03f99a1960e5f565ff72fc95e2230d63b4bd45b3
kenneth-rayner <31237148+kenneth-rayner@users.noreply.github.com>
2021-06-28 12:29:21 +0100

    CTCTRADERS-2320: udate radios for declaration type based on procesudre type and offic

--- app/controllers/DeclarationTypeController.scala
+++ app/controllers/DeclarationTypeController.scala
@@ -61,7 +61,7 @@ class DeclarationTypeController @Inject() (
         "form"   -> preparedForm,
         "mode"   -> mode,
         "lrn"    -> lrn,
-        "radios" -> DeclarationType.radios(preparedForm)
+        "radios" -> DeclarationType.radios(preparedForm, request.userAnswers)()
       )
 
       renderer.render("declarationType.njk", json).map(Ok(_))
@@ -78,7 +78,7 @@ class DeclarationTypeController @Inject() (
               "form"   -> formWithErrors,
               "mode"   -> mode,
               "lrn"    -> lrn,
-              "radios" -> DeclarationType.radios(formWithErrors)
+              "radios" -> DeclarationType.radios(formWithErrors, request.userAnswers)()
             )
 
             renderer.render("declarationType.njk", json).map(BadRequest(_))
