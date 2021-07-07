c07b17b9ad86a5157938c2f3172f4e5afc141594
kenneth-rayner <31237148+kenneth-rayner@users.noreply.github.com>
2021-07-07 07:05:37 +0100

    CTCTRADERS-2345:add navigation

---
+++ test/forms/PrincipalTirHolderIdFormProviderSpec.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.behaviours.StringFieldBehaviours
+import play.api.data.FormError
+
+class PrincipalTirHolderIdFormProviderSpec extends StringFieldBehaviours {
+
+  private val requiredKey      = "principalTirHolderIdPage.error.required"
+  private val lengthKey        = "principalTirHolderIdPage.error.length"
+  private val invalidCharsKey  = "principalTirHolderIdPage.error.characters"
+  private val invalidFormatKey = "principalTirHolderIdPage.error.invalidFormat"
+  private val maxLength        = 35
+  val form                     = new PrincipalTirHolderIdFormProvider()()
+
+  ".value" - {
+
+    val fieldName = "value"
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      stringsWithMaxLength(maxLength)
+    )
+
+    behave like fieldWithMaxLength(
+      form,
+      fieldName,
+      maxLength = maxLength,
+      lengthError = FormError(fieldName, lengthKey, Seq(maxLength))
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+
+    behave like fieldWithInvalidCharacters(form, fieldName, invalidCharsKey, maxLength)
+
+//    "must not bind strings that do not match the eoriNumber format regex" in {
+//      val expectedError          = FormError(fieldName, invalidFormatKey, Seq(principalTirHolderIdFormatRegex))
+//      val generator: Gen[String] = RegexpGen.from("^[//*^&]{1}{1,13}") //TODO update when format is confirmed
+//      forAll(generator) {
+//        invalidString =>
+//          val result: Field = form.bind(Map(fieldName -> invalidString)).apply(fieldName)
+//          result.errors must contain(expectedError)
+//      }
+//    }
+  }
+}
