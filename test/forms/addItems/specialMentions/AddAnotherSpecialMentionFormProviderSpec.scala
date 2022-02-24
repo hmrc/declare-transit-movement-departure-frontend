db904db8658fb007e1106449ab22b405780af4cb
carrielies <carrielies@users.noreply.github.com>
2022-02-24 12:56:54 +0000

    CTCTRADERS-1539 Add Limit to SpecialMentions

--- test/forms/addItems/specialMentions/AddAnotherSpecialMentionFormProviderSpec.scala
+++ test/forms/addItems/specialMentions/AddAnotherSpecialMentionFormProviderSpec.scala
@@ -24,22 +24,41 @@ class AddAnotherSpecialMentionFormProviderSpec extends BooleanFieldBehaviours {
   val requiredKey = "addAnotherSpecialMention.error.required"
   val invalidKey  = "error.boolean"
 
-  val form = new AddAnotherSpecialMentionFormProvider()()
+  val form = new AddAnotherSpecialMentionFormProvider()
 
   ".value" - {
 
     val fieldName = "value"
 
-    behave like booleanField(
-      form,
-      fieldName,
-      invalidError = FormError(fieldName, invalidKey)
-    )
+    "when max limit not hit" - {
+      behave like booleanField(
+        form(true),
+        fieldName,
+        invalidError = FormError(fieldName, invalidKey)
+      )
 
-    behave like mandatoryField(
-      form,
-      fieldName,
-      requiredError = FormError(fieldName, requiredKey)
-    )
+      behave like mandatoryField(
+        form(true),
+        fieldName,
+        requiredError = FormError(fieldName, requiredKey)
+      )
+    }
+
+    "when max limit hit" - {
+      "must bind true" in {
+        val result = form(false).bind(Map(fieldName -> "true"))
+        result.value.value mustBe false
+      }
+
+      "must bind false to true" in {
+        val result = form(false).bind(Map(fieldName -> "false"))
+        result.value.value mustBe false
+      }
+
+      "must bind blank to true" in {
+        val result = form(false).bind(Map.empty[String, String])
+        result.value.value mustBe false
+      }
+    }
   }
 }
