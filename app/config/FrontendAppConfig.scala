bfd9d1be74e127f2732ebf5e08366c0c8137582f
carrielies <carrielies@users.noreply.github.com>
2022-03-02 13:38:23 +0000

    CTCTRADERS-2592 TidyUp

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -68,9 +68,9 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   lazy val maxGuarantees: Int         = configuration.get[Int]("limits.maxGuarantees")
   lazy val maxSeals: Int              = configuration.get[Int]("limits.maxSeals")
   lazy val maxCountriesOfRouting: Int = configuration.get[Int]("limits.maxCountriesOfRouting")
-  lazy val maxSpecialMentions: Int = configuration.get[Int]("limits.maxSpecialMentions")
-  lazy val maxPackages: Int       = configuration.get[Int]("limits.maxPackages")
-  lazy val maxContainers: Int     = configuration.get[Int]("limits.maxContainers")
-  lazy val maxDocuments: Int      = configuration.get[Int]("limits.maxDocuments")
+  lazy val maxSpecialMentions: Int    = configuration.get[Int]("limits.maxSpecialMentions")
+  lazy val maxPackages: Int           = configuration.get[Int]("limits.maxPackages")
+  lazy val maxContainers: Int         = configuration.get[Int]("limits.maxContainers")
+  lazy val maxDocuments: Int          = configuration.get[Int]("limits.maxDocuments")
   lazy val maxPreviousReferences: Int = configuration.get[Int]("limits.maxPreviousReferences")
 }
